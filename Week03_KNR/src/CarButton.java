e88dacb162fdbda1608eec75bb3d2941f7a96a43
sesang <nnn0_3@naver.com>
2023-09-25 23:16:36 +0900

    week 03

---
+++ Week03_KNR/src/CarButton.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CarButton extends JFrame {
+    private JLabel carLabel;
+    private int carX = 50; 
+
+    public CarButton() {
+        setTitle("자동차 이동");
+        setSize(800, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(new BorderLayout());
+
+        ImageIcon carIcon = new ImageIcon("car.png");
+        carLabel = new JLabel(carIcon);
+        carLabel.setHorizontalAlignment(JLabel.CENTER); 
+        add(carLabel, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        add(buttonPanel, BorderLayout.SOUTH);
+
+        JButton leftButton = new JButton("Left");
+        leftButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                moveCarLeft();
+            }
+        });
+        buttonPanel.add(leftButton);
+
+        JButton rightButton = new JButton("Right");
+        rightButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                moveCarRight();
+            }
+        });
+        buttonPanel.add(rightButton);
+
+        setVisible(true);
+    }
+
+    private void moveCarLeft() {
+        carX -= 10;
+        carLabel.setLocation(carX, carLabel.getY());
+    }
+
+    private void moveCarRight() {
+        carX += 10;
+        carLabel.setLocation(carX, carLabel.getY());
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new CarButton();
+            }
+        });
+    }
+}
